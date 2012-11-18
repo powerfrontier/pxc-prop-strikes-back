f426c4652c9217c2adbfffd941cd5abe9fd82ecf
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-18 20:29:18 +0000

    dsf

--- trunk/servcontrol/balanceo.cpp
+++ trunk/servcontrol/balanceo.cpp
@@ -1,40 +1,86 @@
 #include <iostream>
 #include "balanceo.h"
 
-#define TIME 5
-#define ZONES 9
-
 using namespace std;
 
-static char* zona_servidor[ZONES];
+server* zona_servidor[ZONES];
 std::list<server*> servers;
+
+
+int accionServidor(server* server, Datagram ordenes) {
+  // Envia las ordenes del rebalanceo al servidor
+
+}
+
+void anadirCarga(Datagram datos) {
+  //Añade la información de carga a la lista de servidores
+}
+
+double getAverage() {
+  //Obte la mitjana de carrega dels servidors
+   for (int i = 0; i < NSERVERS; ++i) {
+            incr += servers[i]->server_carga.carga_total;
+   }
+   return incr/NSERVERS;
+}
+
+double getStDev() {
+  //Obte la desviacio estandar de la carrega dels servidors
+        double avg = getAverage();
+        double incr = 0;
+        for (int i = 0; i < NSERVERS; ++i) {
+            incr += pow(servers[i]->server_carga.carga_total - avg, 2);
+        }
+        return sqrt(incr / (NSERVERS - 1));
+}
+
+void balanceo() {
+  //ejecuta el algortimo de balanceo
+}
+
+int solicitarCarga(server* server) {
+  connection* c = new TCPConnection();
+  if(c.connect(server->ip)) {
+    res = c.setReceiveCallback(anadirCarga, new Datagram());
+  }
+  else
+    res = -1
+  
+  return res;
+}
 
 volatile int breakflag = 1;
 
 void handle(int sig) {
     //printf("Hello\n");
-    breakflag = !breakflag;
+    breakflag = 1;
 }
+
 
 int main() {
 
 signal(SIGALRM, handle);
- 
+
  while(1) {
-		//contar x segundos
-    alarm(TIME);
-    while(breakflag) {sleep(1);}
-    
-		//para todas los servidores
-    for(int i=0;i<NSERVERS;i++) {
-      connection * c = new TCPConnection();
-      if(c.connect()
+    if(breakflag) {
+        //para todas los servidores
+        int res;  
+        for(int i=0;i<NSERVERS;i++) {
+          //solicitar_carga
+          res = solicitarCarga(it.next());
+          if(res < 0)
+            printf("No responde el server:" %d, it.actual().id);
+        }
+        servers.sort();
+        //ejecutar algoritmo balanceo
+        balanceo();
+        
+        //enviar las ordenes de balanceo necesarias a los servidores
+        
+        //contar TIME segundos
+        breakflag = 0;
+        alarm(TIME);
     }
-			//solicitar_carga()
-    //ejecutar algoritmo balanceo
-		//enviar las ordenes de balanceo necesarias a los servidores
-    
-    
     //para una segunda version implementar un listener que escuche peticiones de emergencia 
 	}
 
