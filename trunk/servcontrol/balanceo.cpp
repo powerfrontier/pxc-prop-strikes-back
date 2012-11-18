e14d42cbf9489142838a86ed38b7972a4cb33151
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-18 19:10:59 +0000

    dsf

--- trunk/servcontrol/balanceo.cpp
+++ trunk/servcontrol/balanceo.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 #include "balanceo.h"
 
+#define TIME 5
+#define ZONES 9
+
 using namespace std;
 
-static char* ip;
+static char* zona_servidor[ZONES];
 std::list<server*> servers;
+
+volatile int breakflag = 1;
+
+void handle(int sig) {
+    //printf("Hello\n");
+    breakflag = !breakflag;
+}
+
 int main() {
 
-	while(1) {
+signal(SIGALRM, handle);
+ 
+ while(1) {
 		//contar x segundos
+    alarm(TIME);
+    while(breakflag) {sleep(1);}
+    
 		//para todas los servidores
+    for(int i=0;i<NSERVERS;i++) {
+      connection * c = new TCPConnection();
+      if(c.connect()
+    }
 			//solicitar_carga()
-		//procesar respuestas
-
+    //ejecutar algoritmo balanceo
+		//enviar las ordenes de balanceo necesarias a los servidores
+    
+    
+    //para una segunda version implementar un listener que escuche peticiones de emergencia 
 	}
 
 	return 0;
