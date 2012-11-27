12fb18541b18121fb22068a94572e1653b9dfc27
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-27 19:21:32 +0000

    resubiendo balanceo

--- trunk/servcontrol/balanceo.h
+++ trunk/servcontrol/balanceo.h
@@ -3,33 +3,13 @@
 
 #include <list>
 #include <vector>
+#include "../instrucciones/SharedStructs.h"
 
 #define MAXDESV 2
 #define NSERVERS 10
 #define TIME 5
 #define ZONES 9
-//
-struct zona_carga {
-	int zona;
-	double carga;
-};
 
-struct server_carga {
-	std::vector<zona_carga> distribucion;
-	double cargaTotal;
-};
-
-struct server {
-  int id;
-  char* ip;
-  server_carga carga;
-  
-  bool operator < (const server& s) const
-  {
-    return (carga.cargaTotal < s.carga.cargaTotal);
-  }
-  
-};
 
 int solicitarCarga(server* server);
 //void anadirCarga(Datagram datos, server* s);
