4f34c4206bf382e639a8af89865f3ea359a950f7
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-27 19:09:33 +0000

    creando implementacion de instruccion SendServerLoad

---
+++ trunk/instrucciones/SendServerLoad.cpp
@@ -0,0 +1,8 @@
+#include <SendServerLoad.h>
+
+
+void exec() {
+  //Inserir la carrega a la carrega del server que toca
+  // s->server_carga = carga;
+  s->server_carga.cargaTotal = carga.cargaTotal;
+}
