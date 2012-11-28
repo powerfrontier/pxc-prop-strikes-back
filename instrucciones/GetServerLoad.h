a015c21f2f9695275f494ca0f338d95a0b174642
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-28 17:53:26 +0000

    actualizando sharedStructs y getServerLoad

--- instrucciones/GetServerLoad.h
+++ instrucciones/GetServerLoad.h
@@ -1,7 +1,10 @@
-class GetServerLoad : public Datagram<GetZoneLoad> {
-  
+#include "../conexion/include/Datagram.h"
+
+class GetServerLoad : public Datagram<GetSeverLoad> {
+
+	//atributos de la clase
 
 public:
-  GetServerLoad: Datagram<GetServerLoad>("GetServerLoad") {}
+	GetServerLoad: Datagram<GetServerLoad>("GetServerLoad") {} //Poner parametro para saber que servidor le envia la petición y luego lo ponga en la respuesta el servidor de juego
 
 };
