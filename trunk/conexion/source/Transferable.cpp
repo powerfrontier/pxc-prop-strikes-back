089d60fcf57d4992081b6099e7240e8447d78957
skuerzo@gmail.com <skuerzo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-28 17:41:39 +0000

    Arreglado un fallo con el cambio de carpetas. Tendría que ser src, no source. Pero da igual, con que tire soy feliz.

---
+++ trunk/conexion/source/Transferable.cpp
@@ -0,0 +1,11 @@
+#include <Transferable.h>
+
+WrongTransferableException::WrongTransferableException(const std::string& what) throw() : mWhat(what) { }
+WrongTransferableException::~WrongTransferableException() throw() { }
+const char* WrongTransferableException::what() const throw() { return mWhat.c_str(); }
+
+TransferableVersionException::TransferableVersionException(const std::string& what) throw() : mWhat(what) { }
+TransferableVersionException::~TransferableVersionException() throw() { }
+const char* TransferableVersionException::what() const throw() { return mWhat.c_str(); }
+
+bool Transferable::validate() const throw() { return true; }
