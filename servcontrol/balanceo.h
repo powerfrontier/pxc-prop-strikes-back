e6988e8ccb68096d8ab8ef05aaa8be5e713060a4
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-28 17:52:00 +0000

    rediseño de balanceo

--- servcontrol/balanceo.h
+++ servcontrol/balanceo.h
@@ -2,23 +2,29 @@
 #define _balanceo_h
 
 #include <list>
-#include <vector>
+
+#include "../conexion/include/Connection.h"
 #include "../instrucciones/SharedStructs.h"
 
 #define MAXDESV 2
 #define NSERVERS 10
 #define TIME 5
 #define ZONES 9
-#define IPLOGIN 192.168.0.1
+#define IPLOGIN "192.168.0.1"
 #define PUERTOLOGIN 3456
-#define IPREDIRECCION 192.168.0.1
+#define IPREDIRECCION "192.168.0.1"
 #define PUERTOREDIRECCION 3457
+#define IPJUEGO1 "192.168.0.1"
+#define IPJUEGO2 "192.168.0.1"
+#define IPJUEGO3 "192.168.0.1"
+#define IPJUEGO4 "192.168.0.1"
 #define PUERTOJUEGO 3458
 
+void inicializarListaServidores();
 void inicializarConexiones();
 void managerConexiones();
 int solicitarCarga(server* server);
 void balanceo();
-void cambioZona(int serverMaxCarga, int posicionZonaACambiar, int serverMinCarga);
+void cambioZona(server* serverMaxCarga, int posicionZonaACambiar, server* serverMinCarga);
 
 #endif
