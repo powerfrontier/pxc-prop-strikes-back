083ef0fc3ddb5f11b0f2b0f0ec85eb330b2b140b
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-11-30 17:50:21 +0000

    versio amb classes, falta canviar moltes coses

--- trunk/servcontrol/balanceo.h
+++ trunk/servcontrol/balanceo.h
@@ -21,10 +21,30 @@
 #define IPJUEGO4 "192.168.0.1"
 #define PUERTOJUEGO "3458"
 
+class Control : Singleton {
+	double getAverage();
+	double getStDev();
+	void balanceHandle();
+	void loadRequestHandle();
+	void writeDownServer();
 
-void inicializarListaServidores();
-void inicializarConexiones();
-void balanceo();
-void cambioZona(server* serverMaxCarga, int posicionZonaACambiar, server* serverMinCarga);
+public:
+	Server zoneServer[ZONES];
+	std::list<Server> servers;
+	std::mutex recievedMutex;
+	int recievedConnectionMask;
+	Connection* loginConnection;  //= new TCPConnection();
+	Connection* routerConnection; // = new TCPConnection();
+	volatile int breakflag; // = 1; //Variable que fa de sincronització pel timeout de l'espera entre rebalancejos
+	volatile int timeout; // = 1; //Variable que fa de sincronització pel timeout de l'espera de peticion
+
+	Control();
+	void initializeServerList();
+	void initializeConnections();
+	void balance();
+	void zoneChange(Server sourceServer, int changedZonePosition, Server destinationServer);
+
+
+}
 
 #endif
--- trunk/servcontrol/instrucciones/SendServerLoad.cpp
+++ trunk/servcontrol/instrucciones/SendServerLoad.cpp
@@ -3,9 +3,11 @@
 void SendServerLoad::exec(Connection* c) const throw(){
 	//Inserir la carrega a la carrega del server que toca i actualitzar la mascara on toca també
 	s->carga = carga;
-	
 	rebut_mutex->lock();
 	*rebut = *rebut || (1 << s->id);
 	rebut_mutex->unlock();
 
 }
+
+
+
