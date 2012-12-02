3b29e4af3e365b47cc79f1c261ffdca29d323829
buiytre@gmail.com <buiytre@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-02 18:02:15 +0000

    chivatos

--- conexion/source/testServer.cpp
+++ conexion/source/testServer.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 
 int main(int argc, char** argv){
+	char buff[64];
+	str.copy(buff, str.size(), 0);
+	printf(buff);
+    printf("Ready");
+    fflush(stdout);
+
 	ConnectionManager* n = new ConnectionManager();
+	str.copy(buff, str.size(), 0);
+	printf(buff);
+    printf("OBJETO ConnectionManager Creado");
+    fflush(stdout);
+
 	n->listen(std::string(argv[1]));
+	str.copy(buff, str.size(), 0);
+	printf(buff);
+    printf("Listen");
+    fflush(stdout);
+
 	while (1){
 	}
 }
