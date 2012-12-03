b74029fb061df105baa22815ac6369a4d979a7ec
buiytre@gmail.com <buiytre@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-03 16:43:01 +0000

    testConnection works but errors everywhere

--- trunk/testConnection/testClient/testClient.cpp
+++ trunk/testConnection/testClient/testClient.cpp
@@ -1,8 +1,15 @@
 #include <string>
 #include "Datagram.h"
 #include "Connection.h"
+#include <TestClasses.h>
+#include <iostream>
 
 int main(int argc, char** argv){
+	//Esto se hace al principio del main (inicialiación de la conexión)
+	//TestProfile es herenccia de TransferableProfile para decirle 
+	TransferableFactory::instance().setProfile(new TestProfile());
+	TransferableFactory::instance().setProtocol("0.1a");
+
 	printf("MainClient(): 1\n");
         fflush(stdout);
 	Connection* n = new TCPConnection();
@@ -11,8 +18,13 @@ int main(int argc, char** argv){
 	n->connect(std::string(argv[1]),std::string(argv[2]));
 	printf("MainClient(): Connected\n");
         fflush(stdout);
-	
+
+	TestTransferableSent* sent = NULL;
+	sent = new TestTransferableSent();
+	sent->m1(5);
+	n->send(*sent);
 	while (1){
+
 	}
 }
 
--- trunk/testConnection/testServer/testServer.cpp
+++ trunk/testConnection/testServer/testServer.cpp
@@ -3,11 +3,19 @@
 #include "ConnectionManager.h"
 #include "Connection.h"
 #include <iostream>
+#include <TestClasses.h>
+#include <iostream>
 
 
 
 
 int main(int argc, char** argv){
+	//Esto se hace al principio del main (inicialiación de la conexión)
+	//TestProfile es herenccia de TransferableProfile para decirle 
+	TransferableFactory::instance().setProfile(new TestProfile());
+	TransferableFactory::instance().setProtocol("0.1a");
+
+
 	printf("Main: Primera instruccion\n");
  	fflush(stdout);
 	ConnectionManager* n = new ConnectionManager();
