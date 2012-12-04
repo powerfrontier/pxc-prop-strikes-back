1981158ef1ef1d303a43861af98cbd34c7c12ac4
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-04 22:27:10 +0000

    actualizadas la ips de juego para que se conecten al 0.0.0.0

--- trunk/servcontrol/include/Control.h
+++ trunk/servcontrol/include/Control.h
@@ -10,15 +10,15 @@
 
 #define MAXDESV 2
 #define NSERVERS 2
-#define REBALANCING_TIME 5
-#define WAITING_RESPONSE_TIME 10
+#define REBALANCING_TIME 4
+#define WAITING_RESPONSE_TIME 2
 #define NZONES 4
-#define IP_LOGIN "192.168.0.1"
+#define IP_LOGIN "192.168.1.1"
 #define PORT_LOGIN "3456"
-#define IP_ROUTER "192.168.0.1"
+#define IP_ROUTER "192.168.1.1"
 #define PORT_ROUTER "3457"
-#define IP_GAME_1 "192.168.0.1"
-#define IP_GAME_2 "192.168.0.1"
+#define IP_GAME_1 "0.0.0.0"
+#define IP_GAME_2 "0.0.0.0"
 #define PORT_GAME "3458"
 
 class Control : public Singleton<Control> {
