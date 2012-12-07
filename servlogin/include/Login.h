8beab0755c1f9f00a814d1c63befb4c5b7fa909b
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-07 18:42:56 +0000

    Fet connection manager

--- servlogin/include/Login.h
+++ servlogin/include/Login.h
@@ -3,12 +3,17 @@
  
 #include <Singleton.h>
 #include <Connection.h>
+#include <ConnectionManager.h>
+
+#define CLIENT_PORT "3456"
+#define CONTROL_PORT "3457"
 
 class Login : public Singleton<Login> {
-
+  ConnectionManager* manager;
 
 public:
-
+	virtual ~Login();
+	void initializeManager();
 
 };
 
--- servlogin/src/Login.cpp
+++ servlogin/src/Login.cpp
@@ -1,7 +1,23 @@
 #include <Login.h>
+#include <LoginProfile.h>
 
+void Login::initializeManager(){
+    manager = new ConnectionManager();
+    manager->listen(CONTROL_PORT);
+    manager->listen(CLIENT_PORT);
+}
+
+Login::~Login(){
+  delete manager;
+}
 
 int main() {
-
-	return 0;
+	//Esto se hace al principio del main (inicialiación de la conexión)
+	//TestProfile es herenccia de TransferableProfile para decirle 
+	TransferableFactory::instance().setProfile(new LoginProfile());
+	TransferableFactory::instance().setProtocol("0.1a");
+	Login::instance().initializeManager();
+	printf("Login: Estoy en listen\n");
+ 	fflush(stdout);
+	while (1){}
 }
