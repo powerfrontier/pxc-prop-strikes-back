a82daf3c8270aa81e0e1169a1d50b7bd7713a4b1
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-07 16:25:08 +0000

    Nova instrucció

---
+++ servcontrol/src/IPServer.cpp
@@ -0,0 +1,32 @@
+#include <ServerLoad.h>
+#include <Control.h>
+#include <IPServer.h>
+
+void IPServerRcvd::exec(Connection* c) const throw(){
+	char ipSend[16];
+	bool final = false;
+	int i = 0;
+	char* ipChar = Control::instance().getIpServerById(idServer);
+	while(!final && i < 16)
+	{
+		ipSend[i] = ipChar[i];		
+		if( ipChar[i] == '\0' ){
+			final = true;
+		}
+		i++;
+	}
+
+	std::list<Server*>::iterator it;	
+	for(it=Control::instance().servers.begin();it!=Control::instance().servers.end();it++) {		
+		if ((*it)->id == idServer) {
+			//IPServerSend* ipServerSend = new IPServerSend(ipSend);
+			//(*it)->c->send(*ipServerSend);		
+		}		
+	}	
+	
+}
+
+IPServerRcvd::~IPServerRcvd(){
+	
+}
+
