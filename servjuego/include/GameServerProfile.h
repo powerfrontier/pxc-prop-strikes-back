abcc05400f2b0defb5584c50b416ecb9153cf404
skuerzo@gmail.com <skuerzo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-07 17:33:54 +0000

    Añadido Makefile para GameServer

---
+++ servjuego/include/GameServerProfile.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_SERVER_PROFILE_H_
+#define _GAME_SERVER_PROFILE_H_
+
+#include <Transferable.h>
+
+
+class GameServerProfile : public TransferableProfile {
+	public:
+	GameServerProfile();
+	virtual ~GameServerProfile();
+};
+
+
+#endif
