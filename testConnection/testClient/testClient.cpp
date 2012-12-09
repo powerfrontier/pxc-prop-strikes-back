b0eb9a48fc0442cf01ea80f97f8cc1e0f5b6caf5
buiytre@gmail.com <buiytre@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-09 19:15:20 +0000

    testClient y testServer dummys

--- testConnection/testClient/testClient.cpp
+++ testConnection/testClient/testClient.cpp
@@ -20,23 +20,24 @@ int main(int argc, char** argv){
         	fflush(stdout);
 
 
-		TestTransferableSent2* sent2 = NULL;
-		sent2 = new TestTransferableSent2();
-		printf("MainClient(): Sending2...\n");
-		fflush(stdout);
-		n->send(*sent2);
-		printf("MainClient(): sent2\n");
-       		fflush(stdout);
-
-
-		TestTransferableSent7* sent7 = NULL;
-		sent7 = new TestTransferableSent7();
-		printf("MainClient(): Sending7...\n");
-		fflush(stdout);
-		n->send(*sent7);
-		printf("MainClient(): sent7\n");
-       		fflush(stdout);
 		while (1){
+			TestTransferableSent2* sent2 = NULL;
+			sent2 = new TestTransferableSent2();
+			printf("MainClient(): Sending2...\n");
+			fflush(stdout);
+			n->send(*sent2);
+			printf("MainClient(): sent2\n");
+	       		fflush(stdout);
+			delete sent2;
+
+			TestTransferableSent7* sent7 = NULL;
+			sent7 = new TestTransferableSent7();
+			printf("MainClient(): Sending7...\n");
+			fflush(stdout);
+			n->send(*sent7);
+			printf("MainClient(): sent7\n");
+	       		fflush(stdout);
+			delete sent7;
 		}
 	}else{
 		std::cout << "NoooooooooooooooooooooooOO!" << std::endl;
