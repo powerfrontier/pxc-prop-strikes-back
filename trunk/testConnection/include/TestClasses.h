513d14c55156939db4c5bbfcdce086cc9450436e
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-12 18:59:48 +0000

    dummy

--- trunk/testConnection/include/TestClasses.h
+++ trunk/testConnection/include/TestClasses.h
@@ -4,34 +4,47 @@
 #include <Datagram.h>
 
 //Orden que se envía
-class TestTransferableSent7 : public Datagram<TestTransferableSent7> {
-	int m_zona;
-	int m_server;
+class TestTransferableSent100 : public Datagram<TestTransferableSent100> {
+  char username[32]; //Nombre de usuario
+  char password[32]; //Contraseña en texto plano, vamos por SSL
 public:
-	TestTransferableSent7();
-	virtual ~TestTransferableSent7();
+	TestTransferableSent100(char* user, char* pwd);
+	virtual ~TestTransferableSent100();
 	
 };
 
-class TestTransferableSent2 : public Datagram<TestTransferableSent2> {
+class TestTransferableSent102 : public Datagram<TestTransferableSent102> {
+    int clientId; //Id del cliente en nuestro sistema
+  int token; //Token de la sesión actual del cliente
 public:
-	TestTransferableSent2();
-	virtual ~TestTransferableSent2();
+	TestTransferableSent102(int idClient,int idToken);
+	virtual ~TestTransferableSent102();
 	
 };
 
 
 //Orden que se recibe
-class TestTransferableRcvd : public Datagram<TestTransferableRcvd> {
-	int m_1;
-	int m_2;
-	double m_3;
-	int m_4;
+class TestTransferableRcvd103 : public Datagram<TestTransferableRcvd103> {
+ int answerCode; //Código de respuesta de logout. 0=OK, 1=Invalid username. 2=Invalid token. El resto si os motiva hacer alguno.
 
 public:
-	TestTransferableRcvd();
-	virtual ~TestTransferableRcvd();
+	TestTransferableRcvd103() : Datagram<TestTransferableRcvd103>("TestTransferableRcvd103"){}
+	virtual ~TestTransferableRcvd103();
 	
+	void exec(Connection* c) const throw();
+};
+
+//Orden que se recibe
+class TestTransferableRcvd101 : public Datagram<TestTransferableRcvd101> {
+  int answerCode; //Código de respuesta al intento de login. 0 = login correcto, 1=Wrong username or password, los demás que os hagan falta.
+  char routerIp[16]; //Ip del router al que se conectará seguida de uno o más caracteres NULL \0 para indicar el final de string. Si no es un login correcto se ignora.
+  char routerPort[5];
+  int clientId; //Id del cliente en nuestro sistema (mapeo directo id-username)
+  int token; //Token de sesión si es un login correcto. Si no, se ignora. Login necesita mandar el mismo token con el id del cliente al router, junto a la zona en la que se encuentra inicialmente
+
+public:
+   TestTransferableRcvd101() : Datagram<TestTransferableRcvd101>("TestTransferableRcvd101"){}
+  virtual ~TestTransferableRcvd101();
 	void exec(Connection* c) const throw();
 };
 
