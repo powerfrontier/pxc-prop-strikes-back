c00a5ac35858578c2d8936f15c9b9f80d59de8c7
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-13 15:47:29 +0000

    dummy

---
+++ trunk/servlogin/dummyClient/src/TestClasses.cpp
@@ -0,0 +1,75 @@
+#include <TestClasses.h>
+#include <iostream>
+#include <Connection.h>
+
+
+TestTransferableSent100::TestTransferableSent100(char* user, char* pwd) 	: Datagram<TestTransferableSent100>("TestTransferableSent100")
+{
+	      int i;
+  		for( i = 0; i < 32; ++i){
+			username[i] = user[i];			
+		}
+		i = 0;
+		for( i = 0; i < 32; ++i){
+			password[i] = pwd[i];
+			if(pwd[i] == '\0' ) {
+			// printf("ladsa\n");
+		std::cout << "lala " << password << std::endl;
+		fflush(stdout); 
+			  return;
+			}
+		}  
+		
+}
+
+TestTransferableSent100::~TestTransferableSent100() {
+
+}
+
+TestTransferableSent102::TestTransferableSent102(int idClient,int idToken) 	: Datagram<TestTransferableSent102>("TestTransferableSent102"), clientId(idClient),token(idToken) {
+
+}
+
+TestTransferableSent102::~TestTransferableSent102() {
+
+}
+
+TestTransferableRcvd101::~TestTransferableRcvd101() {
+
+}
+
+void TestTransferableRcvd101::exec(Connection* c) const throw() {
+	std::cout <<"Ha llegado una instruccion 101: " << std::endl;
+}
+
+
+TestTransferableRcvd103::~TestTransferableRcvd103() {
+
+}
+
+void TestTransferableRcvd103::exec(Connection* c) const throw() {
+	std::cout <<"Ha llegado una instruccion 103 "  << std::endl;
+}
+
+TestProfile::TestProfile() : TransferableProfile(){
+	mCreators.push_back(std::pair<int, TransferableCreator*>(101, new TestTransferableRcvd101::Creator("TestTransferableRcvd101")));
+	mCreators.push_back(std::pair<int, TransferableCreator*>(103, new TestTransferableRcvd103::Creator("TestTransferableRcvd103")));
+
+	mCreatorIds.push_back(std::pair<std::string, int>("TestTransferableSent100", 100));
+	mCreatorIds.push_back(std::pair<std::string, int>("TestTransferableSent102", 102));
+}
+
+TestProfile::~TestProfile() {
+	for(int i = 0; i < mCreators.size(); ++i) delete mCreators[i].second;
+	mCreators.clear();
+}
+/*
+const TransferableProfile::Creators& TestProfile::getCreators(const std::string& protocol) const throw(TransferableVersionException&) {
+	if(protocol == "0.1a") return mCreators;
+	else throw TransferableVersionException("Unknown protocol");
+}
+
+const TransferableProfile::CreatorIds& TestProfile::getCreatorIds(const std::string& protocol) const throw(TransferableVersionException&) {
+	if(protocol == "0.1a") return mCreatorIds;
+	else throw TransferableVersionException("Unknown protocol");
+}*/
---
+++ trunk/servlogin/dummyClient/src/testClient.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <Datagram.h>
+#include <Connection.h>
+#include <TestClasses.h>
+#include <iostream>
+
+int main(int argc, char** argv){
+	//Esto se hace al principio del main (inicialiación de la conexión)
+	//TestProfile es herenccia de TransferableProfile para decirle 
+	TransferableFactory::instance().setProfile(new TestProfile());
+	TransferableFactory::instance().setProtocol("0.1a");
+	std::string ip(argv[1]);
+	std::string port(argv[2]);
+	printf("MainClient(): 1\n");
+        fflush(stdout);
+	Connection* n = new TCPConnection();
+	while(!n->connect(ip, port)) { 
+		std::cout << "No se ha podido conectar. Se volvera a intentar en unos momentos.. "<< std::endl; 
+		sleep(3);
+	}
+	printf("MainClient(): objeto Conexion creado\n");
+        fflush(stdout);
+	printf("MainClient(): Connected\n");
+        fflush(stdout);
+
+	if (n->isLinkOnline()){			
+			TestTransferableSent100* sent100 = NULL;
+			sent100 = new TestTransferableSent100("usuario","pass");
+		
+			printf("MainClient(): Sending100...\n");
+			fflush(stdout);
+			n->send(*sent100);
+			printf("MainClient(): sent100\n");
+	       		fflush(stdout);
+			delete sent100;
+
+			
+			printf("Ahora hacemos logout\n");
+			TestTransferableSent102* sent102 = NULL;
+			sent102 = new TestTransferableSent102(0,0);
+			printf("MainClient(): Sending102...\n");
+			fflush(stdout);
+			n->send(*sent102);
+			printf("MainClient(): sent102\n");
+	       		fflush(stdout);
+			delete sent102;
+		}else{
+			std::cout << "Conexion perdida, Reconectando... "<< std::endl;
+			if(n->connect(ip, port)) {
+				std::cout << "Conexion Restablecida "<< std::endl; 
+			}else{
+				std::cout << "No se ha podido conectar. Se volvera a intentar en unos momentos.. "<< std::endl; 
+				sleep(3);
+			}
+		}
+
+}
+
+
