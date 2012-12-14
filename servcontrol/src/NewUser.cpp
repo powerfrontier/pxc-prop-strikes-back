c857e9b159efb293a9ac78c235d5a55db2ee20a1
dra.bcn@gmail.com <dra.bcn@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-14 16:55:47 +0000

    instrucciones listas

--- servcontrol/src/NewUser.cpp
+++ servcontrol/src/NewUser.cpp
@@ -5,9 +5,19 @@
 
 #include <iostream>
 
+
 void NewUserRcvd::exec(Connection* c) const throw(){
 	
 	std::cout << "me llega una instruccion NewUser" << std::endl;
+	int zone = Control::instance().getZoneDB(idUser); //aconseguim la zona on estaba l'usuari de BD
+	Server* gameServer = Control::instance().zoneServer[zone]; //aconseguim el server que está ara en aquesta zona
+	
+	RouterNewUserSend* intrRouter = new RouterNewUserSend(idUser, token, zone);
+	Control::instance().routerConnection->sendAnswer(*intrRouter);
+	delete(intrRouter);
+	GameNewUserSend* intrGame = new GameNewUserSend(idUser, zone);
+	gameServer->c->sendAnswer(*intrGame);
+	delete(intrGame);
 }
 
 NewUserRcvd::~NewUserRcvd(){
