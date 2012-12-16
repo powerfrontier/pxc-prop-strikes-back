b3ba7a74d970eb64b21319d6935d56e8b9efaecf
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-16 10:55:50 +0000

    login request fet

--- trunk/servlogin/src/LoginRequest.cpp
+++ trunk/servlogin/src/LoginRequest.cpp
@@ -6,19 +6,20 @@
 #include <iostream>
 #include <string.h>
 #include <mutex>
+#include <map>
 
 using namespace std;
 
 void LoginRequestRcvd::exec(Connection* c) const throw(){
   const char* userPointer;
   const char* pwdPointer;
-  
+  std::map<Connection*,int>::iterator it;
   /*Parámetros a pasar*/
   int answerCode; //Código de respuesta al intento de login. 0 = login correcto, 1=Wrong username or password, los demás que os hagan falta.
   char routerIp[16]; //Ip del router al que se conectará seguida de uno o más caracteres NULL \0 para indicar el final de string. Si no es un login correcto se ignora.
   char routerPort[5]; // Puerto del router
-  int clientId; //Id del cliente en nuestro sistema (mapeo directo id-username)
-  int token; //Token de sesión si es un login correcto. Si no, se ignora. Login necesita mandar el mismo token con el id del cliente al router, junto a la zona en la que se encuentra inicialmente
+  int clientId,oldClientId; //Id del cliente en nuestro sistema (mapeo directo id-username)
+  int token,oldToken; //Token de sesión si es un login correcto. Si no, se ignora. Login necesita mandar el mismo token con el id del cliente al router, junto a la zona en la que se encuentra inicialmente
   ///////////////////////
   cout << "Recibimos conexión" << std::endl;
   Login::instance().loginMutex.lock();
@@ -33,33 +34,32 @@ void LoginRequestRcvd::exec(Connection* c) const throw(){
 	  clientId = Login::instance().nextFreeToken;
 	  token = Login::instance().nextFreeToken;
 	  Login::instance().userTokenMap.insert(pair<int,int>(clientId,token));
-	  if(Login::instance().userConnectionMap.count(c) == 0){
+	  it = Login::instance().userConnectionMap.find(c);
+	  if(it == Login::instance().userConnectionMap.end()){
 	    //Insertamos en el mapa de usuario/conexión
-	    Login::instance().userConnectionMap.insert(pair<Connection*,int>(c,clientId));
-	    
+	    Login::instance().userConnectionMap.insert(pair<Connection*,int>(c,clientId));	    
 	  }else{
 	    //Hacemos logout del antiguo usuario
-	    ClientDisconnectSend* clientDisconnectSend = new ClientDisconnectSend(clientId,token);
-	    Login::instance().controlConnection->sendAnswer(*clientDisconnectSend); 
-	    
-	    //Enviamos los nuevos datos de los usuarios a control
+	    // Recogemos los datos antiguos
+	    oldClientId = it->second;
+	    oldToken = Login::instance().userTokenMap.find(oldClientId)->second;
+	    ClientDisconnectSend* clientDisconnectSend = new ClientDisconnectSend(oldClientId,oldToken);
+	    Login::instance().controlConnection->sendAnswer(*clientDisconnectSend); 	    	    
 	  }
 	  Login::instance().nextFreeToken++;
 	  Login::instance().usersConnected++;
   }else{
 	  answerCode = 1;
-  }
-  
+  }  
   //Enviar info a balanceo  
   NewClientSend* newClientSend = new NewClientSend(clientId,token);
   Login::instance().controlConnection->sendAnswer(*newClientSend);  
   cout << "Valor controlconn2: " << Login::instance().controlConnection << endl;
-cout << "port de control: " << Login::instance().controlConnection->getPort() << endl;
+  cout << "port de control: " << Login::instance().controlConnection->getPort() << endl;
   //Enviar info a cliente
   LoginRequestSend* loginRequestSend = new LoginRequestSend(answerCode, routerIp,routerPort, clientId,token);
-	cout << "port de client: " << c->getPort() << endl;
-  c->sendAnswer(*loginRequestSend);
-  
+  cout << "port de client: " << c->getPort() << endl;
+  c->sendAnswer(*loginRequestSend);  
   //delete newClientSend;
   delete loginRequestSend;
   Login::instance().loginMutex.unlock();
