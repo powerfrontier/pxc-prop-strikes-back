2a2794a87abb41ecfdfbf533a37adcddef5697c8
skuerzo@gmail.com <skuerzo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-16 22:31:57 +0000

    DERP

--- servrouter/src/RouterProfile.cpp
+++ servrouter/src/RouterProfile.cpp
@@ -3,7 +3,7 @@
 RouterProfile::RouterProfile() {
 	
 	//Control
-	mCreatorIds.push_back(std::pair<std::string, int>(1, "GetServerIpSend"));
+	mCreatorIds.push_back(std::pair<std::string, int>("GetServerIpSend"), 1);
 	
 	mCreators.push_back(std::pair<int, TransferableCreator*>(6, new GetServerIpRvcd::Creator("GetServerIpRvcd")));
 	mCreators.push_back(std::pair<int, TransferableCreator*>(7, new SetZoneToServerRcvd::Creator("SetZoneToServerRcvd")));
