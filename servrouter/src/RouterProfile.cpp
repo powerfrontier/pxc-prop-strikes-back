838012ed6203722162331a1a2fb8cbb3e8d0a583
skuerzo <skuerzo@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-16 23:13:50 +0000

    Arreglado fallo de copypaste en RouterProfile.cpp

--- servrouter/src/RouterProfile.cpp
+++ servrouter/src/RouterProfile.cpp
@@ -1,9 +1,11 @@
 #include <RouterProfile.h>
+#include <InstControl.h>
+#include <InstServer.h>
 
 RouterProfile::RouterProfile() {
 	
 	//Control
-	mCreatorIds.push_back(std::pair<std::string, int>("GetServerIpSend"), 1);
+	mCreatorIds.push_back(std::pair<std::string, int>("GetServerIpSend", 1));
 	
 	mCreators.push_back(std::pair<int, TransferableCreator*>(6, new GetServerIpRvcd::Creator("GetServerIpRvcd")));
 	mCreators.push_back(std::pair<int, TransferableCreator*>(7, new SetZoneToServerRcvd::Creator("SetZoneToServerRcvd")));
@@ -14,7 +16,7 @@ RouterProfile::RouterProfile() {
 	//mCreators.push_back(std::pair<int, TransferableCreator*>(, ));
 	
 	//Router
-	mCreatorIds.push_back(std::pair<std::string, int>(150, "AddRouterSend"));
+	mCreatorIds.push_back(std::pair<std::string, int>("AddRouterSend", 150));
 	
 	mCreators.push_back(std::pair<int, TransferableCreator*>(155, new EndZoneTransferRcvd::Creator("EndZoneTransferRcvd")));
 	
