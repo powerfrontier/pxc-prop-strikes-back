36eef19229c1ac516f2090da4aa9c670757d38e0
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-16 19:14:30 +0000

    canvi ports

--- servlogin/include/Login.h
+++ servlogin/include/Login.h
@@ -9,7 +9,7 @@
 #include <map>
 
 #define CLIENT_PORT "3450"
-#define CONTROL_PORT "3456"
+#define CONTROL_PORT "3457"
 #define ROUTER_IP "127.0.0.1"
 #define ROUTER_PORT "3500"
 #define IP_LENGTH 16
