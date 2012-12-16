ee5aa6a79362eabfdf9df283bfc262af7efa987d
ivan.bargallo@gmail.com <ivan.bargallo@gmail.com@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-16 14:05:26 +0000

    login amb reconexions i tal, falta fer l'apartat de l'exec en desconexió no controlat

--- servlogin/src/LogoutRequest.cpp
+++ servlogin/src/LogoutRequest.cpp
@@ -10,7 +10,8 @@ void LogoutRequestRcvd::exec(Connection* c) const throw(){
   int answerCode; //Código de respuesta de logout. 0=OK, 1=Invalid username. 2=Invalid token. El resto si os motiva hacer alguno.
   std::map<int,int>::iterator it;
   std::cout << "Usuarios connectats prelogout: " << Login::instance().usersConnected << std::endl;
-  if( Login::instance().userTokenMap.count(clientId) == 0 ){
+  it = Login::instance().userTokenMap.find(clientId);
+  if( it == Login::instance().userTokenMap.end() ){
     answerCode = 1;
   }else{
       it = Login::instance().userTokenMap.find(clientId);
@@ -18,13 +19,19 @@ void LogoutRequestRcvd::exec(Connection* c) const throw(){
 	answerCode = 2;
       }else{
 	//Desconexión correcta
+	std::string username = Login::instance().idToUserMap.find(clientId)->second;
+	Login::instance().userToIdMap.erase(Login::instance().userToIdMap.find(username));
+	Login::instance().idToUserMap.erase(Login::instance().idToUserMap.find(clientId));
+	Login::instance().idToConnectionMap.erase(Login::instance().idToConnectionMap.find(clientId));
+	Login::instance().connectionToIdMap.erase(Login::instance().connectionToIdMap.find(c));
 	Login::instance().userTokenMap.erase(it);
 	answerCode = 0;	
 	Login::instance().usersConnected--;
+	std::cout << "Salida del else de logout" << std::endl;
 	//Enviar info balanceo
-	ClientDisconnectSend* clientDisconnectSend = new ClientDisconnectSend(clientId,token);
-	Login::instance().controlConnection->sendAnswer(*clientDisconnectSend);  
-	delete clientDisconnectSend;
+	//ClientDisconnectSend* clientDisconnectSend = new ClientDisconnectSend(clientId,token);
+	//Login::instance().controlConnection->sendAnswer(*clientDisconnectSend);  
+	//delete clientDisconnectSend;
       }    
   }
   Login::instance().loginMutex.unlock();
