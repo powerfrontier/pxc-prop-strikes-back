924f604ef9dcffcb51812b520e78ba48de54382f
buiytre <buiytre@47f443ec-08e9-9342-46cf-20eb32800c4d>
2012-12-18 01:28:31 +0000

    comitdummy

--- servlogin/dummyClient/src/testClient.cpp
+++ servlogin/dummyClient/src/testClient.cpp
@@ -19,6 +19,7 @@ int main(int argc, char** argv){
 	Connection* n;
 	for( i = 0; i < 100 ; ++i){
 		n = new TCPConnectionSecurity();
+		n->connect(ip, port);
 		conexiones.push_back(n);
 	}
 //	Connection* n = new TCPConnectionSecurity();
@@ -31,45 +32,44 @@ int main(int argc, char** argv){
 	printf("MainClient(): Connected\n");
         fflush(stdout);
 for(i = 0; i < 100; ++i){
-	if (n->isLinkOnline()){			
+//	if (n->isLinkOnline()){			
 	std::ostringstream ostr;
-ostr << i;   
-		std::string numberConnection = ostr.str();
-		std::cout << "Conexion num: " << i << std::endl;	
-		n = conexiones.at(i);
-			TestTransferableSent100* sent100 = NULL;
-			sent100 = new TestTransferableSent100((char*)numberConnection.c_str(),argv[4]);
-		
-			printf("MainClient(): Sending100...\n");
-			fflush(stdout);
-			n->send(*sent100);
-			printf("MainClient(): sent100\n");
-	       		fflush(stdout);
-			delete sent100;
+	ostr << i;   
+	std::string numberConnection = ostr.str();
+	std::cout << "Conexion num: " << i << std::endl;	
+	n = conexiones.at(i);
+	TestTransferableSent100* sent100 = NULL;
+	sent100 = new TestTransferableSent100((char*)numberConnection.c_str(),argv[4]);
 
+	printf("MainClient(): Sending100...\n");
+	fflush(stdout);
+	n->send(*sent100);
+	printf("MainClient(): sent100\n");
+	fflush(stdout);
+	delete sent100;
 			
-			printf("Ahora hacemos logout\n");
-			fflush(stdout);
-			TestTransferableSent102* sent102 = NULL;
-			sent102 = new TestTransferableSent102(0,0);
-			printf("MainClient(): Sending102...\n");
-			fflush(stdout);
-			n->send(*sent102);
-			printf("MainClient(): sent102\n");
-	       		fflush(stdout);
-			delete sent102;
+	printf("Ahora hacemos logout\n");
+	fflush(stdout);
+	TestTransferableSent102* sent102 = NULL;
+	sent102 = new TestTransferableSent102(0,0);
+	printf("MainClient(): Sending102...\n");
+	fflush(stdout);
+	n->send(*sent102);
+	printf("MainClient(): sent102\n");
+	fflush(stdout);
+	delete sent102;
 
 	
 
-		}else{
-			std::cout << "Conexion perdida, Reconectando... "<< std::endl;
-			if(n->connect(ip, port)) {
-				std::cout << "Conexion Restablecida "<< std::endl; 
-			}else{
-				std::cout << "No se ha podido conectar. Se volvera a intentar en unos momentos.. "<< std::endl; 
-				sleep(3);
-			}
-		}
+//		}else{
+//			std::cout << "Conexion perdida, Reconectando... "<< std::endl;
+//			if(n->connect(ip, port)) {
+//				std::cout << "Conexion Restablecida "<< std::endl; 
+//			}else{
+//				std::cout << "No se ha podido conectar. Se volvera a intentar en unos momentos.. "<< std::endl; 
+//				sleep(3);
+//			}
+//		}
       }
 while(1);
 }
